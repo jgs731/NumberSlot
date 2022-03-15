40deb1b047480000555e5245bd18c0537cb837e1
Joshua Smith <jgs73@live.co.uk>
2022-03-15 22:04:22 +0000

    Initial work on project + game - Declared 2D array `gameGrid` - Created for-loop to output a 3x3 grid (with tabbed formatting) abd a random number assigned to each "cell" - Used `GetLength(dimension)` to get length of grid to iterate through nested for-loop

--- Program.cs
+++ Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace NumberSlot
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random random = new Random();
+            int[,] gameGrid = new int[3, 3];
+            
+            for(int i = 0; i < gameGrid.GetLength(0); i++)
+            {
+                for(int j = 0; j < gameGrid.GetLength(1); j++)  
+                {
+                    gameGrid[i, j] = random.Next(0, 3);
+                    Console.Write(gameGrid[i,j] + "\t");
+                }
+                Console.WriteLine("\n");
+            }
         }
     }
 }
