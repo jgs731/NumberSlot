3ae3037fbb336d7c9f8b32e5360ebc32428dc6e1
Joshua Smith <jgs73@live.co.uk>
2022-03-20 13:27:14 +0000

    Added concept of game lines - Default branch uses the middle row for a player playing one line - Implemented horizontal, vertical and diagonal lines based upon the "grid coordinates" TODO: implement these into the game itself

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace NumberSlot
             Random random = new Random();
             int[,] gameGrid = new int[3, 3];
             int playerWager;
+            int[] winningRow = { 2, 2, 2 };
             int winnings = 0;
             
             for(int i = 0; i < gameGrid.GetLength(0); i++)
@@ -23,7 +24,18 @@ namespace NumberSlot
 
             Console.WriteLine("Enter your wager (in whole £): ");
             playerWager = Convert.ToInt32(Console.ReadLine());
-            if (gameGrid[1, 0] == gameGrid[1, 1] && gameGrid[1, 1] == gameGrid[1, 2])
+            int[] topHLine = { gameGrid[0, 0], gameGrid[0, 1], gameGrid[0, 2] };
+            int[] middleHLine = { gameGrid[1, 0], gameGrid[1, 1],gameGrid[1, 2] };
+            int[] bottomHLine = { gameGrid[2, 0], gameGrid[2, 1], gameGrid[2, 2] };
+
+            int[] topVLine = { gameGrid[0, 0], gameGrid[0, 1], gameGrid[0, 2] };
+            int[] middleVLine = { gameGrid[1, 0], gameGrid[1, 1], gameGrid[1, 2] };
+            int[] bottomVLine = { gameGrid[2, 0], gameGrid[2, 1], gameGrid[2, 2] };
+
+            int[] leftDRow = { gameGrid[0, 0], gameGrid[1, 1], gameGrid[2, 2] };
+            int[] middleRow = { gameGrid[0, 2], gameGrid[1, 1], gameGrid[2, 0] };
+
+            if (middleHLine == winningRow)
             {
                 winnings += (playerWager * 3);
                 Console.WriteLine($"Total winnings: £{winnings}");
