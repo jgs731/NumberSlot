23158c467e132a89f341cff7ed4277e93f0448ef
Joshua Smith <jgs73@live.co.uk>
2022-03-31 22:56:16 +0100

    Created new class to store `UI Methods` and referenced in `Program` class - Separate methods to prompt user to enter rows, display winnings and continue spinning - Removed some braces in switch statement for readability purposes

---
+++ UIMethods.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberSlot
+{
+    internal static class UIMethods
+    {
+        public static void PromptUserNumberEntry()
+        {
+            Console.Write("Enter the number of lines you want to play (max 8): ");
+        }
+
+        public static void PrintWinnings(int winAmount)
+        {
+            if (winAmount > 0) // minimum win is £1 for a horizonal line
+            {
+                Console.WriteLine($"You have some winning rows!");
+            }
+            Console.WriteLine($"Total winnings: £{winAmount}");
+        }
+
+        public static bool SpinAgainPrompt()
+        {
+            Console.Write("Spin again? (Y/N) ");
+            string response = Console.ReadLine();
+            if (response.ToUpper() == "N")
+            {
+                return false;
+            }
+            else
+            {
+                Console.Clear(); // clear Console for next spin
+                return true;
+            }
+        }
+    }
+}
