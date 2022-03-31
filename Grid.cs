209b03b06c9bc5396bcc38784ba83e50a1a6c898
Joshua Smith <jgs73@live.co.uk>
2022-03-31 21:31:20 +0100

    Improved game and code structure - Implemented try-catch for invalid user input - app will no longer crash and user-friendly message displayed to user - Handled instances where some horizontal and vertical lines weren't iterating correctly - Improved readability of switch statements in `Program.cs` to avoid "fall-through" - Moved `Grid` class into its own file and updated references in `Program.cs` where necessary

---
+++ Grid.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace NumberSlot
+{
+    public static class Grid
+    {
+        static int[] winningCombo = new int[3] { 2, 2, 2 };
+        static int winningsCount = 0;
+        public static int[,] GenerateGrid()
+        {
+            Random random = new Random();
+            int[,] gameGrid = new int[3, 3];
+            for (int i = 0; i < gameGrid.GetLength(0); i++)
+            {
+                for (int j = 0; j < gameGrid.GetLength(1); j++)
+                {
+                    gameGrid[i, j] = random.Next(0, 3);
+                }
+            }
+            return gameGrid;
+        }
+
+        public static int CalculateHorizontalLines(int[,] slotGrid, int lines)
+        {
+            for (int i = 0; i < lines; i++)
+            {
+                if (slotGrid[i, 0] == winningCombo[0] && slotGrid[i, 1] == winningCombo[1] && slotGrid[i, 2] == winningCombo[2])
+                {
+                    winningsCount++;
+                }
+            }
+            return winningsCount;
+        }
+
+        public static int CalculateVerticalLines(int[,] slotGrid, int lines)
+        {
+            if (lines < 6)
+            {
+                for (int j = 0; j < (lines - slotGrid.GetLength(1)); j++)
+                {
+                    if (slotGrid[0, j] == winningCombo[0] && slotGrid[1, j] == winningCombo[1] && slotGrid[2, j] == winningCombo[2])
+                    {
+                        winningsCount += 2;
+                    }
+                }
+            }
+            else 
+            {
+                for (int j = 0; j < (slotGrid.GetLength(1)); j++)
+                {
+                    if (slotGrid[0, j] == winningCombo[0] && slotGrid[1, j] == winningCombo[1] && slotGrid[2, j] == winningCombo[2])
+                    {
+                        winningsCount += 2;
+                    }
+                }
+            }
+            return winningsCount;
+        }
+
+        public static int CalculateDiagonalLines(int[,] slotGrid, int lines)
+        {
+            if (lines == 8)
+            {
+                if (slotGrid[0, 0] == winningCombo[0] && slotGrid[1, 1] == winningCombo[1] && slotGrid[2, 2] == winningCombo[2])
+                {
+                    winningsCount =+ 3;
+                }
+            }
+
+            if (slotGrid[0, 2] == winningCombo[0] && slotGrid[1, 1] == winningCombo[1] && slotGrid[2, 0] == winningCombo[2])
+            {
+                winningsCount =+ 3;
+            }
+
+            return winningsCount;
+        }
+
+        public static void DisplayGrid(int[,] slotGrid)
+        {
+            for (int i = 0; i < slotGrid.GetLength(0); i++)
+            {
+                for (int j = 0; j < slotGrid.GetLength(1); j++)
+                {
+                    Console.Write(slotGrid[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace NumberSlot
         static void Main(string[] args)
         {
             int[,] slotGrid = new int[3, 3];
+            int numberOfRows;
             int winnings = 0;
             bool continueSpinning = true;
 
@@ -14,170 +15,63 @@ namespace NumberSlot
             {
                 slotGrid = Grid.GenerateGrid();
                 Grid.DisplayGrid(slotGrid);
+               
                 Console.Write("Enter the number of lines you want to play (max 8): ");
-                int numberOfRows = Convert.ToInt32(Console.ReadLine());
-                switch (numberOfRows)
+                try
                 {
-                    case 1:
+                    numberOfRows = Convert.ToInt32(Console.ReadLine());
+                    
+                    switch (numberOfRows)
                     {
-                        winnings += Grid.CalculateHorizontalLines(slotGrid, numberOfRows);
-                        break;
+                        case 1:
+                        case 2:
+                        case 3:
+                            {
+                                winnings += Grid.CalculateHorizontalLines(slotGrid, numberOfRows);
+                                break;
+                            }
+                        case 4:
+                        case 5:
+                        case 6:
+                            {
+                                winnings += Grid.CalculateHorizontalLines(slotGrid, numberOfRows);
+                                winnings += Grid.CalculateVerticalLines(slotGrid, numberOfRows);
+                                break;
+                            }
+                        case 7:
+                        case 8:
+                            {
+                                winnings += Grid.CalculateHorizontalLines(slotGrid, numberOfRows);
+                                winnings += Grid.CalculateVerticalLines(slotGrid, numberOfRows);
+                                winnings += Grid.CalculateDiagonalLines(slotGrid, numberOfRows);
+                                break;
+                            }
                     }
-                    case 2:
-                    {
-                        winnings += Grid.CalculateHorizontalLines(slotGrid, numberOfRows);
-                        break;
-                    }
-                    case 3:
-                    {
-                        winnings += Grid.CalculateHorizontalLines(slotGrid, numberOfRows);
-                        break;
-                    }
-                    case 4:
-                    {
-                        winnings += Grid.CalculateHorizontalLines(slotGrid, numberOfRows);
-                        winnings += Grid.CalculateVerticalLines(slotGrid, numberOfRows);
-                        break;
-                    }
-                    case 5:
-                    {
-                        winnings += Grid.CalculateHorizontalLines(slotGrid, numberOfRows);
-                        winnings += Grid.CalculateVerticalLines(slotGrid, numberOfRows);
-                        break;
-                    }
-                    case 6:
-                    {
-                        winnings += Grid.CalculateHorizontalLines(slotGrid, numberOfRows);
-                        winnings += Grid.CalculateVerticalLines(slotGrid, numberOfRows);
-                        break;
-                    }
-                    case 7:
-                    {
-                        winnings += Grid.CalculateHorizontalLines(slotGrid, numberOfRows);
-                        winnings += Grid.CalculateVerticalLines(slotGrid, numberOfRows);
-                        break;
-                    }
-                    case 8:
-                    {
-                        winnings += Grid.CalculateHorizontalLines(slotGrid, numberOfRows);
-                        winnings += Grid.CalculateVerticalLines(slotGrid, numberOfRows);
-                        winnings += Grid.CalculateDiagonalLines(slotGrid, numberOfRows);
-                        break;
-                    }
-                }
 
-                if (winnings > 1)
-                {
-                    Console.WriteLine($"You have some winning rows!");
-                }
-                Console.WriteLine($"Total winnings: £{winnings}");
+                    if (winnings > 0) // minimum win is £1 for a horizonal line
+                    {
+                        Console.WriteLine($"You have some winning rows!");
+                    }
+                    Console.WriteLine($"Total winnings: £{winnings}");
+                    winnings = 0; // reset winnings after each spin
 
-                Console.Write("Do you want another spin? (Y/N) ");
-                string response = Console.ReadLine();
-                if (response.ToUpper() == "N")
-                {
-                    continueSpinning = false;
+                    Console.Write("Spin again? (Y/N) ");
+                    string response = Console.ReadLine();
+                    if (response.ToUpper() == "N")
+                    {
+                        continueSpinning = false;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                    }
                 }
-                else
+                catch (Exception ex)
                 {
+                    Console.WriteLine($"{ex.Message} - invalid value!");
+                    Thread.Sleep(500);
                     Console.Clear();
                 }
-            }
-        }
-    }
-
-    public static class Grid
-    {
-        static int[] winningCombo = new int[3] { 2, 2, 2 };
-        static int winningsCount = 0;
-        public static int[,] GenerateGrid()
-        {
-            Random random = new Random();
-            int[,] gameGrid = new int[3, 3];
-            for (int i = 0; i < gameGrid.GetLength(0); i++)
-            {
-                for (int j = 0; j < gameGrid.GetLength(1); j++)
-                {
-                    gameGrid[i, j] = random.Next(0, 3);
-                }
-            }
-            return gameGrid;
-        }
-
-        public static int CalculateHorizontalLines(int[,] slotGrid, int lines)
-        {
-            for (int i = 0; i < slotGrid.GetLength(0); i++)
-            {
-                if (lines == 1)
-                {
-                    if (slotGrid[1, 0] == winningCombo[0] && slotGrid[1, 1] == winningCombo[1] && slotGrid[1, 2] == winningCombo[2])
-                    {
-                        winningsCount++;
-                    }
-                }
-                else
-                { 
-                    if (slotGrid[i, 0] == winningCombo[0] && slotGrid[i, 1] == winningCombo[1] && slotGrid[i, 2] == winningCombo[2])
-                    {
-                        winningsCount++;
-                    }
-                }
-            }
-            return winningsCount;
-        }
-
-        public static int CalculateVerticalLines(int[,] slotGrid, int lines)
-        {
-            if (lines > 6)
-            {
-                for (int j = 0; j < (lines - slotGrid.GetLength(0) - slotGrid.GetLength(1)); j++)
-                {
-                    if (slotGrid[0, j] == winningCombo[0] && slotGrid[1, j] == winningCombo[1] && slotGrid[2, j] == winningCombo[2])
-                    {
-                        winningsCount += 2;
-                    }
-                }
-            }
-            else
-            {
-                for (int j = 0; j < (lines - slotGrid.GetLength(1)); j++)
-                {
-                    if (slotGrid[0, j] == winningCombo[0] && slotGrid[1, j] == winningCombo[1] && slotGrid[2, j] == winningCombo[2])
-                    {
-                        winningsCount += 2;
-                    }
-                }
-            }
-            return winningsCount;
-        }
-
-        public static int CalculateDiagonalLines(int[,] slotGrid, int lines)
-        {
-            if (lines == 9)
-            {
-                if (slotGrid[0, 0] == winningCombo[0] && slotGrid[1, 1] == winningCombo[1] && slotGrid[2, 2] == winningCombo[2])
-                {
-                    winningsCount =+ 3;
-                }
-            }
-
-            if (slotGrid[0, 2] == winningCombo[0] && slotGrid[1, 1] == winningCombo[1] && slotGrid[2, 0] == winningCombo[2])
-            {
-                winningsCount =+ 3;
-            }
-
-            return winningsCount;
-        }
-
-        public static void DisplayGrid(int[,] slotGrid)
-        {
-            for (int i = 0; i < slotGrid.GetLength(0); i++)
-            {
-                for (int j = 0; j < slotGrid.GetLength(1); j++)
-                {
-                    Console.Write(slotGrid[i, j] + "\t");
-                }
-                Console.WriteLine();
             }
         }
     }
